7444146089558bb12cdf718c29d0a642db4cfaea
Darwin Schulz <darwinschulz@outlook.com>
2021-02-09 16:00:57 +0100

    Add command line argument (-c) and functionality Add read testYAML.yaml file

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-def helloWorld():
-    print("Hello World!")
+import argparse
+import yaml
+from pathlib import Path
 
-helloWorld()
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--convert', type=Path, metavar='', help='Import .yaml to convert to .svg')
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    if args.convert is not None:
+        file_name = str(args.convert).split('\\')[-1]  # get filename
+        print(file_name)
+        with open(file_name, 'r') as stream:
+            try:
+                print(yaml.safe_load(stream))
+            except yaml.YAMLError as exc:
+                print(exc)
