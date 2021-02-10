cd95b939d5db0677074d4cb7074683092a1eb51d
Tim Thomas <inf3253@hs-worms.de>
2021-02-10 15:45:18 +0100

    Add comments to ArgParser class and set_args function

--- arg_parser.py
+++ arg_parser.py
@@ -3,16 +3,40 @@ from pathlib import Path
 
 
 class ArgParser:
+    """
+    A class for parsing the console arguments
+
+    Methods
+    -------
+    set_args():
+        Adds the needed arguments to the ArgumentParser class
+    """
 
     def __init__(self):
         return
 
     @staticmethod
-    def get_args():
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-i', '--Import', type=Path, metavar='', help='Visualize .yaml file')
-        parser.add_argument('-e', '--export', type=Path, metavar='', help='Export .yaml to .svg file')
-        parser.add_argument('-r', '--run', type=Path, metavar='', help='Visualize local network')
-        parser.add_argument('-g', '--gui', type=Path, metavar='', help='Start NIV Gui')
+    def set_args():
+        """
+        Adds the needed arguments to the ArgumentParser class
+
+            :returns:
+                Argument strings converted to objects and assigned as attributes of the namespace
+        """
+        parser = argparse.ArgumentParser(prog="niv",
+                                         description="Creates a visualization of your network infrastructure",
+                                         add_help=False)
+        parser.add_argument('-h', '--help', action='help',  help='Show this help message and exit')
+        parser.add_argument('-v', '--version', action='version', version='1.0', help="Show program's version "
+                                                                                     "number and exit")
+        parser.add_argument('-r', '--run', type=Path, metavar='', help='Create visualization without exporting '
+                                                                       'any files')
+        parser.add_argument('-e', '--export', type=Path, metavar='', help='Export .yaml and .svg file to given path'
+                                                                          ' or to current directory if no path given')
+        parser.add_argument('-i', '--import', type=Path, metavar='', help='Create visualization with a given .yaml '
+                                                                          'file')
+        parser.add_argument('-g', '--gui', type=Path, metavar='', help='Start niv gui')
+
+        # parser.print_help()
 
         return parser.parse_args()
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import yaml
 from arg_parser import ArgParser
 
+# Create an instance of the ArgParser class
 arg_parser = ArgParser()
-args = ArgParser.get_args()
+# Call the function "set_args"
+args = ArgParser.set_args()
 
 # if __name__ == '__main__':
-# if args.convert is not None:
-#     file_name = str(args.convert).split('\\')[-1]  # get filename
-#     print(file_name)
-#     with open(file_name, 'r') as stream:
-#         try:
-#             print(yaml.safe_load(stream))
-#         except yaml.YAMLError as exc:
-#             print(exc)
+#     if args.convert is not None:
+#         file_name = str(args.convert).split('\\')[-1]  # get filename
+#         print(file_name)
+#         with open(file_name, 'r') as stream:
+#             try:
+#                 print(yaml.safe_load(stream))
+#             except yaml.YAMLError as exc:
+#                 print(exc)
