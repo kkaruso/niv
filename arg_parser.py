5934ff6a366a200b70e495fb7944759bf854ff38
Tim Thomas <inf3253@hs-worms.de>
2021-02-11 15:22:09 +0100

    Change arguments export and import to save and load

--- arg_parser.py
+++ arg_parser.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+import pathlib
+import sys
 from pathlib import Path
 
 
@@ -20,23 +23,37 @@ class ArgParser:
         """
         Adds the needed arguments to the ArgumentParser class
 
-            :returns:
-                Argument strings converted to objects and assigned as attributes of the namespace
+        :return: Argument strings converted to objects and assigned as attributes of the namespace
         """
         parser = argparse.ArgumentParser(prog="niv",
                                          description="Creates a visualization of your network infrastructure",
                                          add_help=False)
-        parser.add_argument('-h', '--help', action='help',  help='Show this help message and exit')
+        parser.add_argument('-h', '--help', action='help', help='Show this help message and exit')
         parser.add_argument('-v', '--version', action='version', version='1.0', help="Show program's version "
                                                                                      "number and exit")
-        parser.add_argument('-r', '--run', type=Path, metavar='', help='Create visualization without exporting '
-                                                                       'any files')
-        parser.add_argument('-e', '--export', type=Path, metavar='', help='Export .yaml and .svg file to given path'
-                                                                          ' or to current directory if no path given')
-        parser.add_argument('-i', '--import', type=Path, metavar='', help='Create visualization with a given .yaml '
-                                                                          'file')
-        parser.add_argument('-g', '--gui', type=Path, metavar='', help='Start niv gui')
+        parser.add_argument('-r', '--run', action='store_true',
+                            help='Create visualization without exporting any files')
+        parser.add_argument('-s', '--save', type=Path, nargs='?', metavar='',
+                            help='Save .yaml and .svg file to given path or to current directory if no path given')
+        parser.add_argument('-l', '--load', type=check_file(), nargs='?', metavar='',
+                            help='Create visualization with a given .yaml file')
+        parser.add_argument('-g', '--gui', action='store_true', help='Start niv gui')
 
         # parser.print_help()
+        print(parser.parse_args('-r -s this/is/a/path'.split()))
 
-        return parser.parse_args()
+        args = parser.parse_args()
+
+        return args
+
+    def check_file(self, string):
+        """
+        Check if file exists in path
+
+        :param string: path to the file as a string
+        :return: path, if it exists
+        """
+        if os.path.isfile(string):
+            return string
+        else:
+            print("No such file '{}'".format(string), file=sys.stderr)
