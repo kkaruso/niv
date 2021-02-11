8515912a8152b7ac7f5895a3c99524c66d158770
Tim Thomas <inf3253@hs-worms.de>
2021-02-11 15:21:19 +0100

    Add test for set_args function

---
+++ test/test_arg_parser.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from arg_parser import ArgParser
+import argparse
+
+
+class TestArgParser(TestCase):
+    """
+    A class for testing functions of ArgParser class
+    """
+
+    def test_set_args(self):
+        """
+        set_args function test
+        """
+        parser = ArgParser()
+        self.assertEqual(argparse.Namespace(run=None, save=None, load=None, gui=None), parser.set_args())
