8d8c128329ef9d2776c011d44a3d372a2268419b
Tim Thomas <inf3253@hs-worms.de>
2021-02-17 15:26:27 +0100

    Implemented arguments and their tests

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-def helloWorld():
-    print("Hello World!")
+import sys
 
-helloWorld()
+import yaml
+from arg_parser import ArgParser
+
+# Create an instance of the ArgParser class
+arg_parser = ArgParser()
+# Call the function "set_args"
+args = arg_parser.set_args(sys.argv[1:])
+
+# if __name__ == '__main__':
+#     if args.convert is not None:
+#         file_name = str(args.convert).split('\\')[-1]  # get filename
+#         print(file_name)
+#         with open(file_name, 'r') as stream:
+#             try:
+#                 print(yaml.safe_load(stream))
+#             except yaml.YAMLError as exc:
+#                 print(exc)
---
+++ yaml_parser.py
@@ -0,0 +1,8 @@
+import yaml
+
+with open("templates/template.yaml", "r") as stream:
+    try:
+        print(yaml.safe_load(stream))
+    except yaml.YAMLError as exc:
+        print(exc)
+
