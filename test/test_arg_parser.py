14b114e858079accb8d005cd084939affc7b66ea
Daniel Grosmayer <inf3245@hs-worms.de>
2021-02-17 11:31:49 +0100

    Fix and refactor tests

--- test/test_arg_parser.py
+++ test/test_arg_parser.py
@@ -54,26 +54,34 @@ class TestArgParser(TestCase):
         self.assertEqual(argparse.Namespace(save=None, load=None, icons=1, detail=1, run=True, gui=True),
                          parser.set_args(["-g", "-r"]))
 
-        self.assertEqual(argparse.Namespace(save="test2.yaml", load=None, icons=1, detail=1, run=True, gui=False),
-                         parser.set_args(["-s", "test2.yaml", "-r"]))
+        self.assertEqual(
+            argparse.Namespace(save=f"./{self.testdirectory_path}test2.svg", load=None, icons=1, detail=1, run=True,
+                               gui=False),
+            parser.set_args(["-s", f"{self.testdirectory_path}test2.svg", "-r"]))
 
-        self.assertEqual(argparse.Namespace(save="test2.yaml", load=None, icons=1, detail=1, run=False, gui=False),
-                         parser.set_args(["-s", "test2.yaml"]))
+        self.assertEqual(
+            argparse.Namespace(save=f"./{self.testdirectory_path}test3.svg", load=None, icons=1, detail=1, run=False,
+                               gui=False),
+            parser.set_args(["-s", f"{self.testdirectory_path}test3.svg"]))
 
-        self.assertEqual(argparse.Namespace(save="test2.yaml", load=None, icons=1, detail=1, run=False, gui=False),
-                         parser.set_args(["--save", "test2.yaml"]))
+        self.assertEqual(
+            argparse.Namespace(save=f"./{self.testdirectory_path}test4.svg", load=None, icons=1, detail=1, run=False,
+                               gui=False),
+            parser.set_args(["--save", f"{self.testdirectory_path}test4.svg"]))
 
-        self.assertEqual(argparse.Namespace(save=None, load="test.svg", icons=1, detail=1, run=False, gui=False),
-                         parser.set_args(["-l", "test.svg"]))
+        self.assertEqual(argparse.Namespace(save=None, load="test.yaml", icons=1, detail=1, run=False, gui=False),
+                         parser.set_args(["-l", "test.yaml"]))
 
-        self.assertEqual(argparse.Namespace(save=None, load="test.svg", icons=1, detail=1, run=False, gui=False),
-                         parser.set_args(["--load", "test.svg"]))
+        self.assertEqual(argparse.Namespace(save=None, load="test.yaml", icons=1, detail=1, run=False, gui=False),
+                         parser.set_args(["--load", "test.yaml"]))
 
-        self.assertEqual(argparse.Namespace(save=None, load="test.svg", icons=1, detail=1, run=False, gui=True),
-                         parser.set_args(["-g", "-l", "test.svg"]))
+        self.assertEqual(argparse.Namespace(save=None, load="test.yaml", icons=1, detail=1, run=False, gui=True),
+                         parser.set_args(["-g", "-l", "test.yaml"]))
 
-        self.assertEqual(argparse.Namespace(save="test2.yaml", load="test.svg", icons=1, detail=1, run=True, gui=True),
-                         parser.set_args(["-r", "-s", "test2.yaml", "-g", "-l", "test.svg"]))
+        self.assertEqual(
+            argparse.Namespace(save=f"./{self.testdirectory_path}test5.svg", load="test.yaml", icons=1, detail=1,
+                               run=True, gui=True),
+            parser.set_args(["-r", "-s", f"{self.testdirectory_path}test5.svg", "-g", "-l", "test.yaml"]))
 
         self.assertEqual(argparse.Namespace(save=None, load=None, icons=2, detail=2, run=False, gui=False),
                          parser.set_args(["-i", "2", "-d", "2"]))
@@ -95,9 +103,9 @@ class TestArgParser(TestCase):
         with self.assertRaises(argparse.ArgumentTypeError):
             parser.is_path_to_yaml_file("test.txt")
 
-        self.assertEqual("test.svg", parser.is_path_to_yaml_file("test.svg"))
+        self.assertEqual("test.yaml", parser.is_path_to_yaml_file("test.yaml"))
 
-        self.assertIsNotNone("test.svg", parser.is_path_to_yaml_file("test.svg"))
+        self.assertIsNotNone("test.yaml", parser.is_path_to_yaml_file("test.yaml"))
 
     def test_save_to_path(self):
         """
