e27515e4cf598d6a417da8efc4907ed9d770f5ab
Tim Thomas <inf3253@hs-worms.de>
2021-02-18 11:10:46 +0100

    Refactor yaml_parser.py into get_yaml() function

--- yaml_parser.py
+++ yaml_parser.py
@@ -1,8 +1,18 @@
 import yaml
 
-with open("templates/template.yaml", "r") as stream:
-    try:
-        print(yaml.safe_load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
 
+def get_yaml(path):
+    """
+    Reads in a .yaml file from a given path
+
+    :param path: path to the .yaml file
+    :return: yaml object or Exception
+    """
+    with open(f"{path}", "r") as stream:
+        try:
+            yaml_parsed = yaml.safe_load(stream)
+            print(yaml_parsed)
+            return yaml_parsed
+        except yaml.YAMLError as exc:
+            print(exc)
+            raise Exception(f"{exc}")
