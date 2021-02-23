5cff804a40174c44ba844399c5d15ceb47373be1
Darwin Schulz <darwinschulz@outlook.com>
2021-02-23 11:10:13 +0100

    Fix tests in test_arg_parser.py  Made parser globally accessible in arg_parser.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from arg_parser import ArgParser
 # Create an instance of the ArgParser class
 arg_parser = ArgParser(sys.argv[1:])
 # Call the function "set_args"
-args = arg_parser.set_args()
+args = arg_parser.get_parser()
 
 print(args.load)
-print("Cock: " + str(arg_parser.get_load()))
+print("Test: " + str(arg_parser.get_load()))
 
