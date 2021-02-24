3a3c016ebf587f3a85d44e77e0d305de99c008bc
Tim Thomas <inf3253@hs-worms.de>
2021-02-24 10:57:57 +0100

    Add text field for each node in template.yaml and integration into build_diagram.py, clean up code and add more comments

--- build_diagram.py
+++ build_diagram.py
@@ -25,33 +25,52 @@ class BuildDiagram:
     # IP Example
     IP = "192.168.x.x"
 
+    # TODO: Check if icon names are valid by somehow comparing them to the classes in osa.py and cisco.py or to the
+    #   icon catalog
+
     def __init__(self, load_path, save_path):
+        # Initialize variables for dynamically getting the values from the .yaml file
+        # TODO: cleanup (delete not needed comments like old prints)
+
+        # Load the .yaml from the given path
         self.yaml = yaml_parser.get_yaml(load_path)
-        # TODO: Implement save_path somehow and cleanup
+
         self.save_path = save_path
         self.output_format = save_path.split('.')[-1]
-        self.filename = save_path.split('/')[-1].split('.')[0]
-        self.full_filename = f"{self.filename}.{self.output_format}"
-        self.save_path_without_file_format = save_path.split('.')[0]
-        print(f"output_format: {self.output_format}")
-        print(f"file_name: {self.filename}")
-        print(f"full_filename: {self.full_filename}")
-        print(f"save_path_without_file_format: {self.save_path_without_file_format}\n")
+        self.filename = os.path.splitext(save_path)[0]
+
         self.link = f"\n<a xlink:href=\"{self.save_path}\"> {self.IP} </a>"
 
-        # Create variables for dynamically getting the values from the .yaml file for creating the graph
+        # Get title of the diagram, the amount of groups and the groups itself
         self.title = self.yaml.get("title").get("text")
         self.group_count = len(self.yaml.get("groups"))
         self.groups = list(self.yaml.get("groups").keys())
+
+        # Go through each group and take the group name as the key and save the list of members as values in
+        # "group_members" dictionary
         self.group_members = {}
         for member in self.yaml.get("groups").keys():
-            # Creates new key value pairs in group_members dict for each group with its members
             self.group_members[f'{member}'] = self.yaml.get('groups')[member].get('members')
+
+        # Get nodes and the amount of nodes
         self.node_count = len(self.yaml.get("icons"))
         self.nodes = list(self.yaml.get("icons").keys())
+
+        # Go through each node and save the text in "nodes_text" dictionary with the node name as the key
+        self.nodes_text = {}
+        for node in self.yaml.get("icons"):
+            self.nodes_text[f'{node}'] = self.yaml.get("icons").get(f'{node}').get('text')
+
+        # Save each endpoint of a connection as a list in "connections" list
         self.connections = []
         for i in range(0, len(self.yaml.get("connections"))):
             self.connections.append(self.yaml.get("connections")[i].get("endpoints"))
+
+        # Just for "debugging"
+        # TODO: delete when finished with the file
+        print(f"output_format: {self.output_format}")
+        print(f"save_path: {self.save_path}")
+        print(f"filename: {self.filename}\n")
 
         print("Created variables from .yaml:")
         print(f"title: {self.title}")
@@ -60,48 +79,47 @@ class BuildDiagram:
         print(f"group_members: {self.group_members}")
         print(f"node_count: {self.node_count}")
         print(f"nodes: {self.nodes}")
+        print(f"nodes_text: {self.nodes_text}")
         print(f"connections: {self.connections}\n")
 
     def create_diagram(self):
         """
-        Builds the diagram
+        Creates the diagram with the right amount of of nodes, clusters and connections
         """
 
         # Create an instance of the Diagram class to create a diagram context
-        with Diagram(f"\n{self.title}", filename=self.save_path_without_file_format, outformat=self.output_format,
+        with Diagram(f"\n{self.title}", filename=self.filename, outformat=self.output_format,
                      show=True):
             instances = []
             nodes_not_in_groups = []
             members = []
 
-            # Dynamically create the amount of nodes that are not a member of a group
             print(f"group_members: {self.group_members}")
             print(f"nodes: {self.nodes}")
 
             # Fill "members" list with all the group members
             for group_name in self.group_members:
                 for member in list(self.group_members.get(group_name)):
-                    print(f"member: {member}")
+                    # print(f"member: {member}")
                     members.append(member)
 
             # If a node is not a member of a group, create it outside of a cluster
             for node in self.nodes:
                 if node not in members:
                     nodes_not_in_groups.append(node)
-                    instances.append(globals()[node](f"{node}" + self.link))
-            # print(f"nodes_not_in_groups: {nodes_not_in_groups}\n")
+                    instances.append(globals()[node](f"{self.nodes_text[node]}" + self.link))
 
             # Dynamically create the amount of groups given by "group_count" with the corresponding group name
             for group_name in self.group_members:
-                print(f"group_name: {group_name}")
+                # print(f"group_name: {group_name}")
                 with Cluster(f"{group_name}"):
                     # Create a node for each member in every group
                     for member in list(self.group_members.get(group_name)):
-                        print(f"member: {member}")
+                        # print(f"member: {member}")
                         # Create an instance of the node class with the "member" name, if not valid print name of not
                         # valid node
                         try:
-                            instances.append(globals()[member](f"{member}" + self.link))
+                            instances.append(globals()[member](f"{self.nodes_text[member]}" + self.link))
                         except KeyError:
                             print(f"KeyError: {member} is not a valid node name!")
 
@@ -159,6 +177,7 @@ class BuildDiagram:
         with open(self.save_path, "w") as file:
             file.write(out_string)
 
+        # TODO: Add logic to only embed links in .svg (and maybe .pdf if it's possible)
         # Only add link to diagram if it's a .svg
         # if self.output_format == "svg":
         self.html_to_ascii()
@@ -166,7 +185,6 @@ class BuildDiagram:
     def html_to_ascii(self):
         """
         Replace html-entity with ascii-symbol for embedding hyperlinks in svg
-        :return:
         """
         fin = open(f"{self.save_path}", "rt")
         data = fin.read()
