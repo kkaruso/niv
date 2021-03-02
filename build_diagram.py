9cf75faa31ffaea187cedbe655fc2dadeb188da1
Tim Thomas <inf3253@hs-worms.de>
2021-03-02 16:31:18 +0100

    Add fill_icon_dictionary() function

--- build_diagram.py
+++ build_diagram.py
@@ -7,7 +7,6 @@
 build_diagram.py
 Dynamically creates the diagram
 """
-import re
 import yaml_parser
 from diagrams import *
 from diagrams.icons.ciscoPng import *
@@ -46,37 +45,48 @@ class BuildDiagram:
         self.title_subtext = self.yaml.get("title").get("subText")
         self.title_font_size = self.yaml.get("title").get("fontSize")
 
-        # TODO: Create function like "fill_dictionary()" or so for the following for loops
+        # TODO: Create function like "fill_dictionary(dict, field)" or so for the following for loops
         # Get name and text of nodes and save as key value pairs in "nodes_text"
         self.nodes_text = {}
-        for node in self.yaml.get("icons"):
-            self.nodes_text[node] = self.yaml.get("icons").get(node).get('text')
-            if self.yaml.get('icons')[node].get('text') is None:
-                self.nodes_text[node] = ""
+        self.fill_icon_dictionary(self.nodes_text, "text")
+        # for node in self.yaml.get("icons"):
+        #     self.nodes_text[node] = self.yaml.get("icons").get(node).get('text')
+        #     if self.yaml.get('icons')[node].get('text') is None:
+        #         self.nodes_text[node] = ""
 
         # Get ip of each node
         self.nodes_ip = {}
-        for node in self.yaml.get("icons"):
-            self.nodes_ip[node] = self.yaml.get("icons").get(node).get('ip')
-            if self.yaml.get('icons')[node].get('ip') is None:
-                self.nodes_ip[node] = ""
+        self.fill_icon_dictionary(self.nodes_ip, "ip")
+        # for node in self.yaml.get("icons"):
+        #     self.nodes_ip[node] = self.yaml.get("icons").get(node).get('ip')
+        #     if self.yaml.get('icons')[node].get('ip') is None:
+        #         self.nodes_ip[node] = ""
 
         # Get port of each node
         self.nodes_port = {}
-        for node in self.yaml.get("icons"):
-            self.nodes_port[node] = self.yaml.get("icons").get(node).get('port')
-            if self.yaml.get('icons')[node].get('port') is None:
-                self.nodes_port[node] = ""
+        self.fill_icon_dictionary(self.nodes_port, "port")
+        # for node in self.yaml.get("icons"):
+        #     self.nodes_port[node] = self.yaml.get("icons").get(node).get('port')
+        #     if self.yaml.get('icons')[node].get('port') is None:
+        #         self.nodes_port[node] = ""
 
-        # Get name and members of groups and save as key value pairs in "group_members"
-        self.group_members = {}
-        for member in self.yaml.get("groups").keys():
-            self.group_members[member] = self.yaml.get('groups')[member].get('members')
+        # Get the URL of each node, clear empty URLs
+        self.nodes_url = {}
+        self.fill_icon_dictionary(self.nodes_url, "url")
+        # for url in self.yaml.get("icons").keys():
+        #     self.nodes_url[url] = self.yaml.get('icons')[url].get('url')
+        #     if self.yaml.get('icons')[url].get('url') is None:
+        #         self.nodes_url[url] = ""
 
         # Save each endpoint of a connection as a list in "connections" list
         self.connections = []
         for i in range(0, len(self.yaml.get("connections"))):
             self.connections.append(self.yaml.get("connections")[i].get("endpoints"))
+
+        # Get name and members of groups and save as key value pairs in "group_members"
+        self.group_members = {}
+        for member in self.yaml.get("groups").keys():
+            self.group_members[member] = self.yaml.get('groups')[member].get('members')
 
         # Get the URL of each group, clear empty URLs
         self.group_url = {}
@@ -84,13 +94,6 @@ class BuildDiagram:
             self.group_url[url] = self.yaml.get('groups')[url].get('url')
             if self.yaml.get('groups')[url].get('url') is None:
                 self.group_url[url] = ""
-
-        # Get the URL of each node, clear empty URLs
-        self.nodes_url = {}
-        for url in self.yaml.get("icons").keys():
-            self.nodes_url[url] = self.yaml.get('icons')[url].get('url')
-            if self.yaml.get('icons')[url].get('url') is None:
-                self.nodes_url[url] = ""
 
         # Only get coordinates from nodes if layout = neato
         if self.graph_layout == "neato":
@@ -132,7 +135,7 @@ class BuildDiagram:
         print(f"nodes_ip: {self.nodes_ip}")
         print(f"connections: {self.connections}\n")
 
-    def create_nodes(self, instances, members):
+    def create_nodes(self, members):
         """
         Create nodes outside and inside of clusters
         """
@@ -158,12 +161,12 @@ class BuildDiagram:
                 for member in list(self.group_members.get(group_name)):
                     self.create_single_node(member)
 
-    def create_connections(self, instances, instance_names):
+    def create_connections(self, instance_names):
         """
         Create connections between nodes
         """
         # Get the names of the instances as strings to create the connections
-        for instance in instances:
+        for instance in self.instances:
             instance_name = str(instance).split('.')[-1].split('>')[0]
             instance_names.append(instance_name)
 
@@ -180,7 +183,7 @@ class BuildDiagram:
                 if instance_names[i] == self.connections[j][0]:
                     for k, _ in enumerate(instance_names):
                         if self.connections[j][1] == instance_names[k]:
-                            _ = instances[k] - instances[i]
+                            _ = self.instances[k] - self.instances[i]
 
     def create_diagram(self):
         """
@@ -204,9 +207,9 @@ class BuildDiagram:
             instance_names = []
 
             # Create nodes and clusters
-            self.create_nodes(self.instances, members)
+            self.create_nodes(members)
             # Create connections
-            self.create_connections(self.instances, instance_names)
+            self.create_connections(instance_names)
 
             # # Group 1
             # with Cluster("Gruppe 1"):
@@ -242,8 +245,8 @@ class BuildDiagram:
         """
         try:
             node_text = f"{self.nodes_text[node]}\n" \
-                        f"IP: {self.nodes_ip[node]}\n" \
-                        f"Port: {self.nodes_port[node]}"
+                        f"{self.nodes_ip[node]}\n" \
+                        f"{self.nodes_port[node]}"
             # Remove double newlines for the case when port is given but no url
             node_text = node_text.replace("\n\n", "\n")
 
@@ -268,3 +271,15 @@ class BuildDiagram:
             print(
                 f"KeyError in {self.load_path}: '{node}' is not given in 'icons', that's why it does "
                 f"not show in the diagram. Add it to 'icons' or remove it as a member.")
+
+    def fill_icon_dictionary(self, _dict, _field):
+        """
+        Fills a given dictionary with information from a field in a .yaml
+
+        :param _dict: the given dictionary to fill
+        :param _field: to fill the dictionary with
+        """
+        for node in self.yaml.get("icons"):
+            _dict[node] = self.yaml.get("icons").get(node).get(_field)
+            if self.yaml.get('icons')[node].get(_field) is None:
+                _dict[node] = ""
