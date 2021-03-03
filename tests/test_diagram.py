25164f0e94f38494ec29464c285f17d39ce1682e
Tim Thomas <inf3253@hs-worms.de>
2021-03-03 15:57:56 +0100

    Fix path

--- tests/test_diagram.py
+++ tests/test_diagram.py
@@ -28,7 +28,7 @@ class TestBuildDiagram(TestCase):
         with Diagram("test", filename="test", outformat="svg", show=False):
             members = []
             diagram = BuildDiagram(
-                os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '\\tests\\test_template.yaml', "", 1)
+                os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/tests/test_template.yaml', "", 1)
             diagram.create_nodes(members)
 
             test_members = ["cloud1", "db1", "deviceScanner1"]
@@ -42,7 +42,7 @@ class TestBuildDiagram(TestCase):
             instances = []
             members = []
             diagram = BuildDiagram(
-                os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '\\tests\\test_template.yaml', "", 1)
+                os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/tests/test_template.yaml', "", 1)
             diagram.create_nodes(members)
             diagram.create_connections(instances)
 
