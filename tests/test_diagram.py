3a9f764924a0f6bef787d8b6595d13bc6caec871
Tim Thomas <inf3253@hs-worms.de>
2021-03-17 14:53:00 +0100

    Delete legacy test

--- tests/test_diagram.py
+++ tests/test_diagram.py
@@ -21,20 +21,6 @@ class TestBuildDiagram(TestCase):
         with open("test.svg", "w"):
             pass
 
-    def test_create_nodes(self):
-        """
-        create_nodes tests
-        """
-        with Diagram("test", filename="test", outformat="svg", show=False):
-            members = []
-            diagram = BuildDiagram(
-                os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/tests/test_template.yaml', "test2.svg",
-                1, False)
-            diagram.create_nodes(members)
-
-            test_members = ["cloud1", "db1", "deviceScanner1"]
-            self.assertEqual(test_members, members)
-
     def test_create_diagram(self):
         print("ICH BIN HIER BEI : ")
         print(os.getcwd())
