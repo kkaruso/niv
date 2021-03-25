5d6587bc60b9803cd5a5c336f3c65739445383bd
Tim Thomas <inf3253@hs-worms.de>
2021-03-25 19:51:37 +0100

    Add navbar button animation

--- pages/src/components/Navbar.js
+++ pages/src/components/Navbar.js
@@ -6,10 +6,10 @@ export default class Navbar extends React.Component {
         return (
             <header>
                 <ul class="navLinks">
-                    <li><a href="#introduction">Über Uns</a></li>
-                    <li><a href="#example">Beispiel</a></li>
-                    <li><a href="#conclusion">Fazit</a></li>
-                    <li><a href="#team">Team</a></li>
+                    <li><a href="#introduction" class="btn btn-intro">Über Uns</a></li>
+                    <li><a href="#example" class="btn btn-example">Beispiel</a></li>
+                    <li><a href="#conclusion" class="btn btn-conclusion">Fazit</a></li>
+                    <li><a href="#team" class="btn btn-team">Team</a></li>
                 </ul>
                 <a class="getItNow" href="https://gitlab.rlp.net/top/21s/niv/niv/-/blob/dev/pages/public/introduction/introduction.md" target="_blank" rel="noreferrer">
                     <button>Installieren</button>
